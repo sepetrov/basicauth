9e55de2ca06a691fc3cc91d3dadb5d3257a198e8
Stanislav Petrov <s.e.petrov@gmail.com>
2016-05-17 13:22:12 +0100

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # basicauth
+
+**basicauth** is a simple HTTP Basic authentication middleware, written in [Go](https://golang.org/).
---
+++ basicauth_test.go
@@ -0,0 +1,101 @@
+package basicauth
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type headers map[string]string
+type users map[string]string
+type provider struct {
+	u map[string]string
+}
+
+func (p provider) Find(u []byte) (Credentials, error) {
+	if p, ok := p.u[string(u)]; ok {
+		return Credentials{User: u, Password: []byte(p)}, nil
+	}
+	return Credentials{}, fmt.Errorf("Can not find user %s", u)
+}
+
+var h http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {}
+var tests = [...]struct {
+	users users
+	req   headers
+	code  int
+	res   headers
+}{
+	{
+		users: users{},
+		req:   headers{},
+		code:  http.StatusUnauthorized,
+		res:   headers{"WWW-Authenticate": `Basic realm="` + realm + `"`},
+	},
+	{
+		users: users{},
+		req:   headers{"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte("user:password"))},
+		code:  http.StatusUnauthorized,
+		res:   headers{"WWW-Authenticate": `Basic realm="` + realm + `"`},
+	},
+	{
+		users: users{"foo": "bar"},
+		req:   headers{"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte("user:password"))},
+		code:  http.StatusUnauthorized,
+		res:   headers{"WWW-Authenticate": `Basic realm="` + realm + `"`},
+	},
+	{
+		users: users{"foo": "bar", "user": "password"},
+		req:   headers{"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte("user:password"))},
+		code:  http.StatusOK,
+		res:   headers{"WWW-Authenticate": ""},
+	},
+}
+
+func TestProtect(t *testing.T) {
+	for i, test := range tests {
+		p := provider{test.users}
+		w := httptest.NewRecorder()
+		r, err := http.NewRequest("GET", "", nil)
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		for k, v := range test.req {
+			r.Header.Add(k, v)
+		}
+		http.HandlerFunc(Protect(h, p)).ServeHTTP(w, r)
+		if test.code != w.Code {
+			t.Errorf("#%d got code %d, want %d ", i, w.Code, test.code)
+		}
+		for k, v := range test.res {
+			if v != w.Header().Get(k) {
+				t.Errorf("#%d got header \"%s: %s\", want \"%s: %s\"", i, k, v, k, w.Header().Get(k))
+			}
+		}
+	}
+}
+
+func TestBasicAuthProtect(t *testing.T) {
+	for i, test := range tests {
+		auth := New(provider{test.users})
+		w := httptest.NewRecorder()
+		r, err := http.NewRequest("GET", "", nil)
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		for k, v := range test.req {
+			r.Header.Add(k, v)
+		}
+		http.HandlerFunc(auth.Protect(h)).ServeHTTP(w, r)
+		if test.code != w.Code {
+			t.Errorf("#%d got code %d, want %d ", i, w.Code, test.code)
+		}
+		for k, v := range test.res {
+			if v != w.Header().Get(k) {
+				t.Errorf("#%d got header \"%s: %s\", want \"%s: %s\"", i, k, v, k, w.Header().Get(k))
+			}
+		}
+	}
+}
